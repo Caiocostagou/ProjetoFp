e9b9b824e0f8672ddac2b7ef62690b204d2862e5
[viniinitc] <[vinitcavalcantena@gmail.com]>
2025-05-07 23:44:48 -0300

    Update esqueleto.py
    comecei a funcao para adicionar os treinos, porem esta incompleta(nao ta escrevendo no txt os dados)

--- esqueleto.py
+++ esqueleto.py
@@ -1,6 +1,20 @@
 import os
 def create():
     pass
+def create(tiposetreinos, exercises):
+    tipo = input("Qual tipo de treino voce deseja adicionar: AMRAP(AM), EMOM(EM), For Time(FT)?\n").upper()
+    
+    if tipo == "AM":
+        nometreino = input("Qual o nome do treino?\n")
+        tempo = int(input(f"Qual a duracao, em minutos, do treino de {nometreino}:\n"))
+        tiposetreinos["AMRAP"].append(nometreino)
+        exercises[nometreino] = tempo
+        with open("hora.txt", "w") as app:
+            return app.write(f"{nometreino}: {tempo}")
+        
+
+
+
 def read():
     pass
 def update():
@@ -9,6 +23,7 @@ def delete():
     pass
 def CRUD(acao):
     while acao != "C" or acao != "C" or acao != "C" or acao != "C":
+    while acao != "C" and acao != "R" and acao != "U" and acao != "D":
         acao = input("adicionar um treino (C)\nVisualizar seus treinos atuais (R)\nEditar seus treinos atuais (U)\nExcluir algum de seus treinos(D)\n").upper()
         if acao == "C":
             return create()
@@ -19,14 +34,28 @@ def CRUD(acao):
         elif acao == "D":
             return delete()
         else:
+    if acao == "C":
+        return create(tiposetreinos,exercises)
+    elif acao == "R":
+        return read()
+    elif acao == "U":
+        return update()
+    elif acao == "D":
+        return delete()
+    else:
             print("\nAcao invalida tente novamente\n")
 exercises = {}
+
+tipo = ""
+
+nometreino = ""
 
 acao = ""
 
 name = ("Thiago")
 
 tiposetreinos = {"AMRAP": "", "EMOM": "", "For Time": ""}
+tiposetreinos = {}
 
 try:
     app = open ("hora.txt", "x")
@@ -34,6 +63,8 @@ try:
     print(f"Ola {name}, esse eh o seu mais novo WOD Tracker\nQual sera sua proxima acao?")
     CRUD(acao)
         
+    
 except:
+<<<<<<< Updated upstream
     print(f"Ola {name}, bem vindo de volta...")
     os.remove("hora.txt")
